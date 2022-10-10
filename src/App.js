0f35f07bbf44abc8f5a486f02add7123a24ddeee
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-10-10 04:18:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/index'
 import Home from './components/Home'
+import About from './components/About/About'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 function App() {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <title>Hello World</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </HelmetProvider>
   )
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+
+const About = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Hello World</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
+      Hello from About
+    </>
+  )
+}
+
+export default About
